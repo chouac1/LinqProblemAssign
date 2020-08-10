b77fd50cbde5c1ac6ff54243296054bf194b4825
Work & School <chouac@live.com>
2020-08-10 12:04:00 -0500

    First problem solved with/out LINQ. Moving on to problem #2.

--- LinqProblemsAssignment/Program.cs
+++ LinqProblemsAssignment/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,18 +11,37 @@ namespace LinqProblemsAssignment
     {
         static void Main(string[] args)
         {
-            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
-            foreach (var word in words)
+
+            //---------------------------------------- Question 1 ----------------------------------------
+
+            //---------------------------------------- without LINQ ----------------------------------------
+            //List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            //List<string> wordsWithTh = new List<string>();
+            //foreach (var word in words)
+            //{
+            //    if (word.Contains("th"))
+            //    {
+            //        wordsWithTh.Add(word);
+            //    }
+            //}
+            //foreach (var word in wordsWithTh)
+            //{
+            //    Console.WriteLine(word);
+            //    Console.ReadLine();
+            //}
+
+
+            //---------------------------------------- with LINQ ----------------------------------------
+            var words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            var wordsWithTh = words.Where(w => w.Contains("th"));
+            foreach (var word in wordsWithTh)
             {
-                if (word.Contains("th"))
-                {
-                    Console.WriteLine(word);
-                    Console.ReadLine();
-                }
+                Console.WriteLine(word);
+                Console.ReadLine();
             }
 
-
-
+            //---------------------------------------- Question 2 ----------------------------------------
+            var names = new List<string>
         }
     }
 }
