89a36d05b2a0c4cfcec848e2af426fc3076ab4a8
Work & School <chouac@live.com>
2020-08-10 20:20:48 -0500

    Question #3 has been further modified and function can now convert the strings into individual strings, string to int, and int to a sorted list.

--- LinqProblemsAssignment/Program.cs
+++ LinqProblemsAssignment/Program.cs
@@ -12,26 +12,8 @@ namespace LinqProblemsAssignment
         static void Main(string[] args)
         {
 
-            //---------------------------------------- Question 1 ----------------------------------------
+            ////---------------------------------------- Question 1 ----------------------------------------
 
-            //---------------------------------------- without LINQ ----------------------------------------
-            //List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
-            //List<string> wordsWithTh = new List<string>();
-            //foreach (var word in words)
-            //{
-            //    if (word.Contains("th"))
-            //    {
-            //        wordsWithTh.Add(word);
-            //    }
-            //}
-            //foreach (var word in wordsWithTh)
-            //{
-            //    Console.WriteLine(word);
-            //    Console.ReadLine();
-            //}
-
-
-            //---------------------------------------- with LINQ ----------------------------------------
             //var words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
             //var wordsWithTh = words.Where(w => w.Contains("th"));
             //foreach (var word in wordsWithTh)
@@ -40,14 +22,31 @@ namespace LinqProblemsAssignment
             //    Console.ReadLine();
             //}
 
-            //---------------------------------------- Question 2 ----------------------------------------
-            var names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
-            var noDuplicateNames = names.Distinct();
-            foreach (var name in noDuplicateNames)
-            {
-                Console.WriteLine(name);
-                Console.ReadLine();
-            }
+            //////---------------------------------------- Question 2 ----------------------------------------
+            //var names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
+            //var noDuplicateNames = names.Distinct();
+            //foreach (var name in noDuplicateNames)
+            //{
+            //    Console.WriteLine(name);
+            //    Console.ReadLine();
+            //}
+
+            //---------------------------------------- Question 3 ----------------------------------------
+            var classGrades = new List<string>() { "80,100,92,89,65", "93,81,78,84,69", "73,88,83,99,64", "98,100,66,74,55" };
+            var classGrades1 = classGrades.Select(g => g.Split(',')).ToList();
+            var classGrades2 = classGrades1.Select(g => g.Select(h => int.Parse(h)).ToList()).ToList(); //'sort,' drop the lowest value
+            var sortedList = classGrades2.Select(h => h.OrderBy(i => i).ToList()).ToList();
+
+            //---------------------------------------- Test ----------------------------------------
+
+            //var list = new List<int> { 5, 4, 3, 7, 9, 8, 6, 1, 0, 2 };
+            //var sortedList = list.OrderBy(a => a);
+            //foreach (var item in sortedList)
+            //{
+            //    Console.WriteLine(item);
+            //    Console.ReadLine();
+            //}
+
 
         }
     }
